7af57016d7c5f08a86393cce59f59217f6c26df4
Nicholas Ricci <naricci@email.neit.edu>
2017-08-24 20:23:14 -0400

    eslint --init

--- public/views/contactus.php
+++ public/views/contactus.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>Contact Us</title>
-        <!-- Bootstrap CSS -->
-        <link href="bower_components/bootstrap/dist/css/bootstrap-theme.css" rel="stylesheet" />
-        <link href="bower_components/bootstrap/dist/css/bootstrap.css" rel="stylesheet" />
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Contact Us</title>
+    <!-- Bootstrap CSS -->
+    <link href="../../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../../bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet" />
+</head>
+<body>
+
+    <!-- FOOTER -->
+    <footer class="navbar-static-bottom navbar-inverse">
+        <div class="container">
+            <ul class="nav navbar-nav">
+                <li><p style="color:white;">© 2017 The Stoop.</p></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><p><a href="#">Back to top</a></p></li>
+                <li><p><a href="aboutus.php">About Us</a></p></li>
+                <li><p><a href="gallery.php">Gallery</a></p></li>
+                <li><p><a href="news.php">News</a></p></li>
+                <li><p><a href="shop.php">Shop</a></p></li>
+                <li><p><a href="contactus.php">Contact Us</a></p></li>
+            </ul>
+        </div><!-- /.container -->
+    </footer>
 
 
-        <!-- jQuery -->
-        <script src="bower_components/jquery/dist/jquery.js"></script>
-        <!-- Bootstrap JavaScript Plugins -->
-        <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
-    </body>
+    <!-- jQuery -->
+    <script src="../../bower_components/jquery/dist/jquery.js"></script>
+    <!-- Bootstrap JavaScript Plugins -->
+    <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../js/holder.min.js"></script>
+    <script src="../js/ie10-viewport-bug-workaround.js"></script>
+    <script src="../js/popper.min.js"></script>
+</body>
 </html>
